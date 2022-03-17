4dedaec479efc6a79af1382d0cef8c40deaf0e72
Mandy Sia <65438576+Mandy-13@users.noreply.github.com>
2022-03-17 14:14:49 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+import main.LandingPage;
+
+//run this class to start the application
+public class Main {
+    public static void main(String[] args) {
+        new LandingPage().setVisible(true);
+    }
+}
